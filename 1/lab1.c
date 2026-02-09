87b24fa1ea1dbbb0e9eb744f35366c8317e1a55b
Ilia <icepower@internet.ru>
2026-02-09 18:16:12 +0300

    Add coments to lab1

--- 1/lab1.c
+++ 1/lab1.c
@@ -14,11 +14,11 @@ void *proc1(void *arg)
     thread_info *ti = (thread_info *)arg;
     while (ti->flag == 0)
     {
-        printf("%c", ti->sym);
-        fflush(stdout);
-        sleep(1);
+        printf("%c", ti->sym); /* вывод символа на стандартный вывод */
+        fflush(stdout);        /* принудительная очистка буфера стандартного вывода для немедленного отображения символа */
+        sleep(1);              /* приостановка выполнения потока на 1 секунду */
     }
-    pthread_exit((void *)1);
+    pthread_exit((void *)1); /* завершение потока с возвратом кода завершения 1 */
 }
 
 void *proc2(void *arg)
@@ -26,11 +26,11 @@ void *proc2(void *arg)
     thread_info *ti = (thread_info *)arg;
     while (ti->flag == 0)
     {
-        printf("%c", ti->sym);
-        fflush(stdout);
-        sleep(1);
+        printf("%c", ti->sym); /* вывод символа на стандартный вывод */
+        fflush(stdout);        /* принудительная очистка буфера стандартного вывода для немедленного отображения символа */
+        sleep(1);              /* приостановка выполнения потока на 1 секунду */
     }
-    pthread_exit((void *)2);
+    pthread_exit((void *)2); /* завершение потока с возвратом кода завершения 2 */
 }
 
 int main()
@@ -43,23 +43,25 @@ int main()
     ti2.flag = 0;
     ti2.sym = '2';
 
-    pthread_create(&ti1.id, NULL, proc1, &ti1);
+    pthread_create(&ti1.id, NULL, proc1, &ti1); /* создание потока 1: ti1.id - дескриптор потока, NULL - атрибуты по умолчанию, proc1 - поточная функция, &ti1 - передаваемый параметр */
     printf("Поток 1 создан\n");
-    pthread_create(&ti2.id, NULL, proc2, &ti2);
+
+    pthread_create(&ti2.id, NULL, proc2, &ti2); /* создание потока 2: ti2.id - дескриптор потока, NULL - атрибуты по умолчанию, proc2 - поточная функция, &ti2 - передаваемый параметр */
     printf("Поток 2 создан\n");
 
     printf("Ожидание нажатия клавиши\n");
-    getchar();
+    getchar(); /* чтение одного символа из стандартного ввода, блокирует выполнение до нажатия Enter */
     printf("Клавиша нажата\n");
 
     ti1.flag = 1;
     ti2.flag = 1;
+
     int *result1, *result2;
 
-    pthread_join(ti1.id, (void **)&result1);
+    pthread_join(ti1.id, (void **)&result1); /* ожидание завершения потока 1: ti1.id - дескриптор потока, result1 - указатель для получения кода завершения потока */
     printf("Поток 1 остановлен\n");
 
-    pthread_join(ti2.id, (void **)&result2);
+    pthread_join(ti2.id, (void **)&result2); /* ожидание завершения потока 2: ti2.id - дескриптор потока, result2 - указатель для получения кода завершения потока */
     printf("Поток 2 остановлен\n");
 
     printf("Код завершения 1 потока: %p\n", result1);
